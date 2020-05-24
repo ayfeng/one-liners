8b11d53a529cea90ceb91dc02d5564d9fb4b2525
Andy Feng <andy@andy.com>
2020-05-24 03:13:19 -0700

    Add script to scan all hosts for an open port

---
+++ oscp/scan-hosts-for-port.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+    cat << EOF
+Usage: $(basename $0) PORTNUMBER SCANDIR
+
+Common ports:
+    21   - FTP
+    22   - SSH
+    23   - Telnet
+    25   - SMTP
+    69   - TFTP
+    80   - HTTP
+    88   - Kerberos
+    110  - Pop3
+    111  - Rpcbind
+    139  - SMB
+    143  - IMAP
+
+    443  - HTTPS
+    445  - Samba
+    631  - CUPS
+    993  - IMAP Encrypted
+    3306 - MySQL
+    3389 - RDP
+    8080 - Various (e.g. Tomcat)
+EOF
+fi
+
+scandir=$1
+port=$2
+
+if [[ ! -d "$scandir" ]]; then
+    echo "SCANDIR does not exist. Exiting..."
+    exit
+fi
+
+for f in $(grep -r '^'$port'/\(tcp\|udp\)' -l "$scandir"); do head -1 "$f" | sed 's/.* \([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\)/\1/'; done | grep -v '/[0-9]*$' | sort -t . -k 3,3n -k 4,4n | uniq
