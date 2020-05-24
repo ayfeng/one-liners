fe48cd4f19aaf6e6102f2e5283311d1cd73f37b1
Andy Feng <andy@andy.com>
2020-05-24 03:13:47 -0700

    Add script to detect endpoints on a web server

--- util/find-endpoints.sh
+++ util/find-endpoints.sh
@@ -3,3 +3,5 @@
 # Description: Find endpoints in Angular project
 
  for f in $(grep -r '\.state(' . -l | sort | uniq); do grep 'url:' $f; done | sed 's/\s\s*//g' | cut -d':' -f2 | tr -d "\"\'," | sed 's@/$@@' | sort | uniq
+
+ for f in $(grep -r '\.state(' . -l | sort | uniq); do grep -H 'url:' -B2 $f; done | awk 'BEGIN { RS=".state\\(" } { gsub(",","",$1); print $1":"$6 }' | tail -n +2 | grep -v modules
